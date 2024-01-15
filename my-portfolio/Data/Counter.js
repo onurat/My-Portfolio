0ac2630031074a013270ad2c5ac41c566e18a3fe
onurat <114289826+onurat@users.noreply.github.com>
2024-01-15 11:10:12 +0000

    Update Counter.js

--- my-portfolio/Data/Counter.js
+++ my-portfolio/Data/Counter.js
@@ -1,5 +1,5 @@
 const counterItems = [
-  { icon: "bi bi-globe", count: 57, duration: 5, title: "Projects" },
+  { icon: "bi bi-globe", count: 6, duration: 5, title: "Projects" },
   { icon: "bi-code-slash", count: 3, duration: 5, title: "Certficates" },
 ];
 
