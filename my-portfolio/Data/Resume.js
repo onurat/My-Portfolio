dbe30bbf9e89035257cd51f04cc02ff109c005b6
onurat <114289826+onurat@users.noreply.github.com>
2024-02-14 18:26:24 +0000

    Update Resume.js

--- my-portfolio/Data/Resume.js
+++ my-portfolio/Data/Resume.js
@@ -19,15 +19,15 @@ const experience = [
     time: "2023",
     location: "London",
     points: ["I developed the Video Collection App, a platform for users to explore and manage a collection of videos.", "I used React for front-end development, Node.js and Express for the backend, and PostgreSQL for the database, showcasing my full-stack development skills. "],
-    links: [],
+    links: ["http://cyfvcollector.s3-website-us-east-1.amazonaws.com/"],
   },
   {
-    title: "Animal-Farm",
+    title: "Booking-Form",
     company: "CodeYourFuture",
     time: "2023",
     location: "London",
     points: ["I developed a calendar system for volunteers and workers to facilitate bookings. Utilizing React, JavaScript, HTML, and CSS.", "The system includes a user-friendly Booking Form and a dynamic Calendar, simplifying the booking process. On the server side, an Express application connects to a PostgreSQL database."],
-    links: [],
+    links: ["https://booking-system-0yfn.onrender.com"],
   },
   {
     title: "Supreme Autoglazing",
@@ -35,7 +35,7 @@ const experience = [
     time: "2023",
     location: "London",
     points: ["I am creating a user-friendly website for Supreme Autoglazing, a London-based windscreen and glass replacement company.", "The ongoing project emphasizes responsive design and intuitive navigation. I used React and HTML/CSS for the front-end development of the website.","Some future changes will allow users to contact the company on the website and an updated design."],
-    links: [],
+    links: ["https://supreme-autoglazing.onrender.com/"],
   },
 ];
 
